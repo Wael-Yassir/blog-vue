e991e1ecf4adced56ddc0b2e529bc920c0af9980
Waelyassir <wael.yassir@gmail.com>
2021-11-05 16:40:47 +0200

    Create Footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="text-center text-lg-start bg-light text-muted">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" target="_blank" href="https://github.com/Wael-Yassir">WAEL YASSIR</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+    footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
