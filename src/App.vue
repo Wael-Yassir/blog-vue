471915499145907fece33fb50fa6c159d058428a
Waelyassir <wael.yassir@gmail.com>
2021-11-05 23:47:15 +0200

    Add Nav-Bar-Item and its integration

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <Nav-Bar-Item />
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
     <Footer-Item />
   </div>
 </template>
@@ -30,18 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/components/Nav-Bar-Item.vue
+++ src/components/Nav-Bar-Item.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>
+    <div style='margin-bottom: 45px;'>
         <b-navbar type="dark" variant="success">
             <b-icon flip-h="false" icon="vector-pen"/>
             <b-navbar-brand href="#">|BLOGGER</b-navbar-brand>
+            <div id="nav">
+                <router-link to="/">Home</router-link> |
+                <router-link to="/about">About</router-link>
+            </div>
         </b-navbar>
     </div>
 </template>
@@ -20,8 +24,25 @@ export default {
         letter-spacing: 10px;
         font-weight: 500;
     }
+
     svg.bi-vector-pen {
         margin-left: 10px;
         color: white;
+    }
+
+    #nav a {
+        color: white;
+        text-decoration: none;
+        font-family: 'Zen Kurenaido', sans-serif;
+        letter-spacing: 3px;
+        font-weight: 500;
+        font-size: 20px;
+        margin: 0 5px;
+    }
+
+    #nav {
+        color: white;
+        margin-left: auto;
+        margin-right: 5px;
     }
 </style>
