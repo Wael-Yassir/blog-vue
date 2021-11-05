f695cd6ac7cc1ad81d5fc566a7e3ef52b9a419c1
Waelyassir <wael.yassir@gmail.com>
2021-11-05 16:40:25 +0200

    Create Card-Item-List

---
+++ src/components/Card-Item-List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="container">
+        <Card-Item class="_card" v-for="(article, index) in articles" :key="index"
+        :title="article.title"
+        :image="article.image"
+        :summary="article.summary"
+        :date="article.date"
+        />
+    </div>
+</template>
+
+<script>
+
+import CardItem from '../components/Card-Item.vue'
+
+export default {
+  name: 'Card-Item-List',
+  props: ['articles'],
+  components: {
+    CardItem
+  }
+}
+</script>
+
+<style scoped>
+#container {
+    display: flex;
+    flex-wrap: wrap;
+}
+._card {
+    margin: 4px 8px;
+}
+</style>
